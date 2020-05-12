33f68e1abf78ad10032539758509fe0235b5a5fc
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-12 19:07:28 +0530

    sieve of eratosthenes, TLE

---
+++ src/main/java/com/pranavacharya/codeforces/OracandFactors.java
@@ -0,0 +1,56 @@
+package com.pranavacharya.codeforces;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class OracandFactors {
+
+    HashMap<Integer, Integer> cache;
+
+    public OracandFactors() {
+        this.cache = new HashMap();
+    }
+
+    public int findFactors(int n, int k) {
+        while (k > 0) {
+            n = n + fFunction(n);
+            k--;
+        }
+        return n;
+    }
+
+    public int fFunction(int n) {
+        if (this.cache.containsKey(n)) {
+            return this.cache.get(n);
+        }
+        boolean[] numbers = new boolean[n + 1];
+        numbers[1] = true;
+        for (int i = 2; i * i < numbers.length; i++) {
+            if (numbers[i]) {
+                continue;
+            }
+            for (int j = 2; i * j < numbers.length; j++) {
+                numbers[i * j] = true;
+            }
+        }
+        for (int i = 1; i < numbers.length; i++) {
+            if (!numbers[i] && n % i == 0) {
+                this.cache.put(n, i);
+                return i;
+            }
+        }
+        this.cache.put(n, n);
+        return n;
+    }
+
+    public static void main(String args[]) {
+        OracandFactors oaf = new OracandFactors();
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt();
+        for (int i = 1; i <= testcases; i++) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            System.out.println(oaf.findFactors(n, k));
+        }
+    }
+}
