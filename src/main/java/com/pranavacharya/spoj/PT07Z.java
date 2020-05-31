7df2ecd5a4e43f8e36b9d9053fc4a41229c8f287
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-31 22:31:23 +0530

    2 dfs, first to find a leave in max, then longest path from that leaf

--- src/main/java/com/pranavacharya/spoj/PT07Z.java
+++ src/main/java/com/pranavacharya/spoj/PT07Z.java
@@ -1,57 +1,38 @@
 package com.pranavacharya.spoj;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PT07Z {
 
-    private int max;
+    private int[] visited;
+    private int maxD;
+    private int maxNode;
 
-    public PT07Z() {
-        this.max = 0;
+    public int findLengthLongestPath(int nodes, ArrayList<Integer>[] adj) {
+        maxD = -1;
+        maxNode = -1;
+        visited = new int[nodes + 1];
+        dfs(adj, visited, 1, 0);
+
+        Arrays.fill(visited, 0);
+        maxD = -1;
+        dfs(adj, visited, maxNode, 0);
+
+        return maxD;
     }
 
-    public int findLengthLongestPath(int nodes, int[][] edges) {
-        HashMap<Integer, ArrayList<Integer>> adj = new HashMap();
-        int[] inorder = new int[nodes + 1];
-        for (int[] e : edges) {
-            ArrayList<Integer> temp = adj.getOrDefault(e[0], new ArrayList());
-            temp.add(e[1]);
-            adj.put(e[0], temp);
-            inorder[e[1]]++;
+    private void dfs(ArrayList<Integer>[] adj, int[] visited, int node, int depth) {
+        visited[node] = 1;
+        if (depth > maxD) {
+            maxD = depth;
+            maxNode = node;
         }
-        for (int i = 1; i <= nodes; i++) {
-            if (inorder[i] == 0) {
-                dfs(adj, i, new ArrayList());
+        for (int nei : adj[node]) {
+            if (visited[nei] == 0) {
+                dfs(adj, visited, nei, depth + 1);
             }
-        }
-        return this.max;
-    }
-
-    private int dfs(HashMap<Integer, ArrayList<Integer>> adj, int node, ArrayList<Integer> path) {
-        if (!adj.containsKey(node)) {
-            return 0;
-        } else {
-            path.add(node);
-            ArrayList<Integer> neighbour = adj.get(node);
-            int d = 0;
-            int local = 0;
-            ArrayList<Integer> l = new ArrayList();
-            for (int n : neighbour) {
-                int value = dfs(adj, n, path) + 1;
-                l.add(value);
-                local = Math.max(local, value);
-            }
-            Collections.sort(l);
-            d += l.get(l.size() - 1);
-            if (l.size() > 1) {
-                d += l.get(l.size() - 2);
-            }
-            max = Math.max(max, d);
-            path.remove(path.size() - 1);
-            return local;
         }
     }
 
@@ -59,11 +40,16 @@ public class PT07Z {
         PT07Z p = new PT07Z();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] edges = new int[n - 1][2];
-        for (int i = 0; i < n - 1; i++) {
-            edges[i][0] = sc.nextInt();
-            edges[i][1] = sc.nextInt();
+        ArrayList<Integer>[] adj = new ArrayList[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            adj[i] = new ArrayList();
         }
-        System.out.println(p.findLengthLongestPath(n, edges));
+        for (int i = 0; i < n - 1; i++) {
+            int first = sc.nextInt();
+            int second = sc.nextInt();
+            adj[first].add(second);
+            adj[second].add(first);
+        }
+        System.out.println(p.findLengthLongestPath(n, adj));
     }
 }
