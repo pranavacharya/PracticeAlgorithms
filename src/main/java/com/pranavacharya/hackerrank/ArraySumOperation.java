73c4ca1e7196f42642ea2d8ca8acdd7cb666e8ff
Pranav Acharya <apranav.acharya@gmail.com>
2020-08-03 22:26:32 +0530

    linear solution

--- src/main/java/com/pranavacharya/hackerrank/ArraySumOperation.java
+++ src/main/java/com/pranavacharya/hackerrank/ArraySumOperation.java
@@ -1,36 +1,25 @@
 package com.pranavacharya.hackerrank;
 
 import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 
 public class ArraySumOperation {
 
     public List<Long> performOperations(int N, List<Integer> op) {
         List<Long> result = new ArrayList();
-        Deque<Integer> dqueue = new LinkedList();
-        HashSet<Integer> set = new HashSet();
-        long sum = 0;
-        for (int i = 1; i <= N; i++) {
-            dqueue.add(i);
-            set.add(i);
-            sum += i;
-        }
+        long n = N;
+        long sum = ((n * (n + 1)) / 2);
+        int first = 1;
+        int last = N;
         for (int i : op) {
-            if (set.contains(i)) {
-                int first = dqueue.pollFirst();
-                int last = dqueue.pollLast();
-                dqueue.addFirst(last);
-                dqueue.addLast(first);
+            if (i == first || i == last || (i > 1 && i < N)) {
+                int temp = last;
+                last = first;
+                first = temp;
             } else {
-                int last = dqueue.pollLast();
                 sum -= last;
-                set.remove(last);
-                dqueue.addLast(i);
-                set.add(i);
                 sum += i;
+                last = i;
             }
             result.add(sum);
         }
