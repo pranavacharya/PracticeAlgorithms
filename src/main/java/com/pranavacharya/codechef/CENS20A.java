ce3f767f7359dff7c16bc39a68cfa17fb37b3115
Pranav Acharya <apranav.acharya@gmail.com>
2020-08-21 03:45:47 +0530

    difference array, 2D update query, fast reader

--- src/main/java/com/pranavacharya/codechef/CENS20A.java
+++ src/main/java/com/pranavacharya/codechef/CENS20A.java
@@ -1,60 +1,95 @@
 package com.pranavacharya.codechef;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class CENS20A {
 
-    public void formMatrix(int[][] matrix, int[][] query) {
-        for (int[] q : query) {
-            int rows = q[0];
-            int rowe = q[2];
-            int cols = q[1];
-            int cole = q[3];
-            for (int i = rows; i <= rowe; i++) {
-                for (int j = cols; j <= cole; j++) {
-                    if (matrix[i][j] == 0) {
-                        matrix[i][j] = 1;
-                    } else {
-                        matrix[i][j] = 0;;
-                    }
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
+            return st.nextToken();
         }
-        printMatrix(matrix);
-    }
 
-    public void printMatrix(int[][] matrix) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[i].length; j++) {
-                sb.append(matrix[i][j]).append(" ");
-            }
-            sb.append("\n");
+        int nextInt() {
+            return Integer.parseInt(next());
         }
-        System.out.println(sb.toString());
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 
     public static void main(String args[]) {
-        CENS20A c = new CENS20A();
-        Scanner sc = new Scanner(System.in);
+        FastReader sc = new FastReader();
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] matrix = new int[n + 1][m + 1];
+        int[][] da = new int[matrix.length + 1][matrix[0].length + 1];
         for (int i = 1; i <= n; i++) {
             String line = sc.next();
             int j = 1;
             for (char ch : line.toCharArray()) {
-                matrix[i][j++] = ch == '0' ? 0 : 1;
+                matrix[i][j++] = ch - '0';
             }
         }
         int q = sc.nextInt();
-        int[][] query = new int[q][4];
         for (int i = 0; i < q; i++) {
-            query[i][0] = sc.nextInt();
-            query[i][1] = sc.nextInt();
-            query[i][2] = sc.nextInt();
-            query[i][3] = sc.nextInt();
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            da[x1][y1]++;
+            da[x2 + 1][y2 + 1]++;
+            da[x2 + 1][y1]--;
+            da[x1][y2 + 1]--;
         }
-        c.formMatrix(matrix, query);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[i].length; j++) {
+                da[i][j] += da[i - 1][j] + da[i][j - 1] - da[i - 1][j - 1];
+                if (da[i][j] % 2 == 0) {
+                    sb.append(matrix[i][j]);
+                } else {
+                    if (matrix[i][j] == 1) {
+                        sb.append(0);
+                    } else {
+                        sb.append(1);
+                    }
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
     }
 }
