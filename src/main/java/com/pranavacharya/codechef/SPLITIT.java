77c6aaf24170aebe5c11f53127f711b9e0a50fd7
Pranav Acharya <apranav.acharya@gmail.com>
2020-10-19 23:05:24 +0530

    O(N) solution

--- src/main/java/com/pranavacharya/codechef/SPLITIT.java
+++ src/main/java/com/pranavacharya/codechef/SPLITIT.java
@@ -1,12 +1,58 @@
 package com.pranavacharya.codechef;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class SPLITIT {
 
+    static class FastReader {
+
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
     public String isPossible(String input) {
-        for (int i = 1; i < input.length(); i++) {
-            if (input.substring(0, i).contains(input.substring(i))) {
+        for (int i = 0; i < input.length() - 1; i++) {
+            if (input.charAt(i) == input.charAt(input.length() - 1)) {
                 return "YES";
             }
         }
@@ -15,7 +61,7 @@ public class SPLITIT {
 
     public static void main(String args[]) {
         SPLITIT s = new SPLITIT();
-        Scanner sc = new Scanner(System.in);
+        FastReader sc = new FastReader();
         StringBuilder sb = new StringBuilder();
         int t = sc.nextInt();
         for (int i = 0; i < t; i++) {
