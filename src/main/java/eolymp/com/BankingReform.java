b376e48e981a0d33e8cf1a157004e17c773fc232
Pranav Acharya <apranav.acharya@gmail.com>
2020-12-04 02:56:34 +0530

    dynamic programming

---
+++ src/main/java/eolymp/com/BankingReform.java
@@ -0,0 +1,42 @@
+package eolymp.com;
+
+import java.util.Scanner;
+
+public class BankingReform {
+
+    public int findCombination(int n) {
+        int[] coins = new int[]{1, 5, 10, 25, 50};
+        int[][] dp = new int[coins.length][n + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (coins[i] == 1) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                if (coins[i] == j) {
+                    dp[i][j]++;
+                }
+                if (j - coins[i] >= 0) {
+                    dp[i][j] += dp[i - 1][j] + dp[i][j - coins[i]];
+                } else {
+                    dp[i][j] += dp[i - 1][j];
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+
+    public static void main(String args[]) {
+        BankingReform br = new BankingReform();
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        while (sc.hasNextLine()) {
+            String total = sc.nextLine();
+            if (total.isEmpty()) {
+                break;
+            }
+            sb.append(br.findCombination(Integer.parseInt(total))).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
