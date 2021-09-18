48c1cdd0b740e37826f38d59228612b3f5480ac6
Pranav Acharya <apranav.acharya@gmail.com>
2021-09-18 14:36:24 -0400

    linear solution, time complexity O(N), space complexity O(N)

--- src/main/java/com/pranavacharya/kickstart/TrashBins.java
+++ src/main/java/com/pranavacharya/kickstart/TrashBins.java
@@ -4,20 +4,44 @@ import java.util.Scanner;
 
 public class TrashBins {
 
-    private int findDist(String s) {
+    private long findDist(String s) {
         char[] arr = s.toCharArray();
-        int n = arr.length;
-        int min = 0;
-        for (int i = 0; i < n; i++) {
-            int local = Integer.MAX_VALUE;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == '1') {
-                    local = Math.min(local, Math.abs(j - i));
+        int n = s.length();
+        long[] left = new long[n + 1];
+        long[] right = new long[n + 1];
+        int last = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '1') {
+                left[i + 1] = 0;
+                last = i;
+            } else {
+                if (last == -1) {
+                    left[i + 1] = Integer.MAX_VALUE;
+                } else {
+                    left[i + 1] = left[i] + 1;
                 }
             }
-            min += local;
         }
-        return min;
+        last = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == '1') {
+                right[i] = 0;
+                last = i;
+            } else {
+                if (last == -1) {
+                    right[i] = Integer.MAX_VALUE;
+                } else {
+                    right[i] = right[i + 1] + 1;
+                }
+            }
+        }
+//        System.out.println(Arrays.toString(left));
+//        System.out.println(Arrays.toString(right));
+        long max = 0;
+        for (int i = 0; i < n; i++) {
+            max += Math.min(left[i + 1], right[i]);
+        }
+        return max;
     }
 
     public static void main(String[] args) {
