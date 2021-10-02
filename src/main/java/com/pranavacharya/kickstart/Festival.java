c40b1cfb2bef73b21f2650ed1171a054f7a1e20e
Pranav Acharya <apranav.acharya@gmail.com>
2021-10-02 04:11:18 -0400

    tree set solution, time complexity O(D + NlogN)

--- src/main/java/com/pranavacharya/kickstart/Festival.java
+++ src/main/java/com/pranavacharya/kickstart/Festival.java
@@ -1,26 +1,74 @@
 package com.pranavacharya.kickstart;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Festival {
 
-    private int maxHappy(int d, int k, int[][] arr) {
-        Arrays.sort(arr, (a, b) -> (b[0] - a[0]));
-        int max = 0;
+    private long maxHappy(int d, int k, int[][] arr) {
+        long max = 0;
+        ArrayList<Integer>[] start = new ArrayList[d + 2];
+        ArrayList<Integer>[] end = new ArrayList[d + 2];
+        for (int i = 0; i < start.length; i++) {
+            start[i] = new ArrayList();
+            end[i] = new ArrayList();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            start[arr[i][1]].add(i);
+            end[arr[i][2] + 1].add(i);
+        }
+        // min heap -> set
+        TreeSet<int[]> top = new TreeSet<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+
+        // max heap -> set
+        TreeSet<int[]> available = new TreeSet<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+
+        long runningSum = 0;
         for (int i = 1; i <= d; i++) {
-            int count = 0;
-            int local = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if (count == k) {
-                    break;
-                }
-                if (i >= arr[j][1] && i <= arr[j][2]) {
-                    local += arr[j][0];
-                    count++;
+
+            // adding event
+            for (int j = 0; j < start[i].size(); j++) {
+                int index = start[i].get(j);
+                available.add(new int[]{arr[index][0], index});
+            }
+
+            // removing event
+            for (int j = 0; j < end[i].size(); j++) {
+                int index = end[i].get(j);
+                available.remove(new int[]{arr[index][0], index});
+
+                boolean isPresent = top.remove(new int[]{arr[index][0], index});
+                if (isPresent) {
+                    runningSum -= arr[index][0];
                 }
             }
-            max = Math.max(max, local);
+
+            // maintaining k elements in top if available
+            while (top.size() < k && !available.isEmpty()) {
+                int[] currMax = available.first();
+                top.add(currMax);
+                runningSum += currMax[0];
+
+                available.remove(currMax);
+            }
+
+            // balancing the heap -> set
+            while (!top.isEmpty() && !available.isEmpty() && top.first()[0] < available.first()[0]) {
+                int[] currMax = available.first();
+                top.add(currMax);
+                runningSum += currMax[0];
+
+                available.remove(currMax);
+
+                int[] topMin = top.first();
+                top.remove(topMin);
+                runningSum -= topMin[0];
+
+                available.add(topMin);
+            }
+
+            max = Math.max(max, runningSum);
         }
         return max;
     }
